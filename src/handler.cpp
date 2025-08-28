0385eec1480018290cfbe1ffdaebcee1a4d7c14d
rushiphalle <rushikeshgphalle@gmail.com>
2025-08-28 22:47:14 +0530

    bug fixes

--- src/handler.cpp
+++ src/handler.cpp
@@ -56,7 +56,6 @@ void streamMonitor(const char* monitorCode, float values[30], int elementsInArra
     strncat(jsonBuffer, temp, sizeof(jsonBuffer) - strlen(jsonBuffer) - 1);
 
     // Print JSON string
-    Serial.println(jsonBuffer);
     MyWebserver::getInstance().publish("MONITOR", jsonBuffer);
 }
 
